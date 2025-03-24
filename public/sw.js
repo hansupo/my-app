12aeb0f04d3797eac4dea6750740de5c1afeb6ae
Hans Harald Pilv <hanspilv@gmail.com>
2025-03-24 02:28:27 +0200

    initial

---
+++ public/sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'xgym-v1'
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        '/',
+        '/manifest.json',
+        '/icons/icon-48x48.png',
+        '/icons/icon-72x72.png',
+        '/icons/icon-96x96.png',
+        '/icons/icon-128x128.png',
+        '/icons/icon-144x144.png',
+        '/icons/icon-152x152.png',
+        '/icons/icon-192x192.png',
+        '/icons/icon-256x256.png',
+        '/icons/icon-384x384.png',
+        '/icons/icon-512x512.png',
+      ])
+    })
+  )
+})
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request)
+    })
+  )
+}) 
